2bdc75f82e0fccb24d274dffaa9433a477b00e58
AalleexxFfrr <dmitrievaleksandr2001@gmail.com>
2023-10-21 01:47:21 +0200

    refactoring: add title, comments

--- keyfilter.c
+++ keyfilter.c
@@ -1,4 +1,8 @@
-// gcc -std=c11 -Wall -Wextra -Werror keyfilter.c -o keyfilter
+/*
+ * keyfilter.c
+ * Theme: Text Processing
+ * Author: Aleksandr Dmitriev, October 2023
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,7 +10,11 @@
 #include <strings.h>
 #include <ctype.h>
 
-
+/*
+Help function to create string to store allowed letters on keyboard
+And also it checks if string doesn't calready contain letter
+To avoid letters repetetivness 
+*/
 void addCharToStringIfNotPresent(char *str, char newChar) {
     int isCharInString = 0;
     for (int i = 0; str[i] != '\0'; i++) {
@@ -23,6 +31,10 @@ void addCharToStringIfNotPresent(char *str, char newChar) {
     }
 }
 
+/*
+I can't use sort functions, so I wrote help function
+To sort output in alphabetical order, using simple bubble sort
+*/
 void alphabetizeString(char *str) {
     int length = strlen(str);
 
@@ -37,61 +49,70 @@ void alphabetizeString(char *str) {
     }
 }
 
-
+/*
+ * Main function
+ * @param argc int
+ * @param *argv[] char
+ */
 int main(int argc, char *argv[]) {
     const char *PREFIX = (argc >= 2) ? argv[1] : "";
     const int PREFIXLENGTH = strlen(PREFIX);
 
     char line[100]; // A buffer to store each line
 
-    // User's input includes only executable file and database
-    // User doesn't provide address, so we display all start letters of adresses
-    // So user can know where to start to search
+    /* User's input includes only executable file and database
+       User doesn't provide address, so we display all possible start letters of adresses
+       So user can know where to start to search
+    */
     if (argc < 2) {
         char allowedLetters[100];
+
         while (fgets(line, sizeof(line), stdin) != NULL) {
             int length = strlen(line);
-            if (length > 0) {
+
+            if (length > 0)
                 addCharToStringIfNotPresent(allowedLetters, line[0]);
-            }
         }
+
         alphabetizeString(allowedLetters);
         printf("Enable: %s\n", allowedLetters);
     }
 
     // User's input includes address or part of it
-    if (argc >= 2) {
-        char allowedLetters[100];
-        char foundedCity[100];
-        int countPrefix = 0;
-        while (fgets(line, sizeof(line), stdin) != NULL) {
+    char allowedLetters[100];
+    char foundedCity[100]; // A buffer to store copy of line, if countPrefix == 1 (only one address in database)
+    int countPrefix = 0;
 
-            int length = strlen(line);
-            if (length > 0) {
-                if (strncasecmp(PREFIX,line, PREFIXLENGTH) == 0) {
-                    strcpy(foundedCity, line);
-                    addCharToStringIfNotPresent(allowedLetters, toupper(line[PREFIXLENGTH]));
-                    countPrefix++;
-                }       
-            }
+    // Stream processing
+    while (fgets(line, sizeof(line), stdin) != NULL) {
+
+        int length = strlen(line);
+
+        if (length > 0) {
+            if (strncasecmp(PREFIX,line, PREFIXLENGTH) == 0) {
+                strcpy(foundedCity, line);
+                addCharToStringIfNotPresent(allowedLetters, toupper(line[PREFIXLENGTH]));
+                countPrefix++;
+            }       
         }
-
-        alphabetizeString(allowedLetters);
-        if (countPrefix == 0)
-            printf("Not found\n");
-
-        if (countPrefix == 1) {
-            printf("Found: ");
-            for (int i = 0; foundedCity[i] != '\0'; i++) {
-                printf("%c", toupper(foundedCity[i]));
-            }
-            printf("\n");   
-        }
-
-        if (countPrefix >= 2)
-            printf("Enable: %s\n", allowedLetters);
     }
-       
-    return 0;
 
+    alphabetizeString(allowedLetters);
+
+    if (countPrefix == 0)
+        printf("Not found\n");
+
+    if (countPrefix == 1) {
+        printf("Found: ");
+        for (int i = 0; foundedCity[i] != '\0'; i++) {
+            printf("%c", toupper(foundedCity[i]));
+            }
+        printf("\n");   
+    }
+
+    if (countPrefix >= 2)
+        printf("Enable: %s\n", allowedLetters);
+
+     return 0;
 }
+       
