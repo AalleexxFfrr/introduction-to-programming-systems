57315ee883826a852a3689037da842368756493c
AalleexxFfrr <dmitrievaleksandr2001@gmail.com>
2023-10-22 00:10:14 +0200

    refactoring: final edition, remove useless if statement

--- keyfilter.c
+++ keyfilter.c
@@ -12,7 +12,7 @@
 
 /*
 Help function to create string to store allowed letters on keyboard
-And also it checks if string doesn't calready contain letter
+And also it checks if string doesn't already contain letter
 To avoid letters repetetivness 
 */
 void addCharToStringIfNotPresent(char *str, char newChar) {
@@ -55,30 +55,15 @@ void alphabetizeString(char *str) {
  * @param *argv[] char
  */
 int main(int argc, char *argv[]) {
+    /* If user's input includes only executable file and database, we take address as empty string
+       User doesn't provide address, so we display all possible start letters of adresses
+       So user can know where to start to search 
+    */
     const char *PREFIX = (argc >= 2) ? argv[1] : "";
     const int PREFIXLENGTH = strlen(PREFIX);
 
     char line[100]; // A buffer to store each line
 
-    /* User's input includes only executable file and database
-       User doesn't provide address, so we display all possible start letters of adresses
-       So user can know where to start to search
-    */
-    if (argc < 2) {
-        char allowedLetters[100];
-
-        while (fgets(line, sizeof(line), stdin) != NULL) {
-            int length = strlen(line);
-
-            if (length > 0)
-                addCharToStringIfNotPresent(allowedLetters, line[0]);
-        }
-
-        alphabetizeString(allowedLetters);
-        printf("Enable: %s\n", allowedLetters);
-    }
-
-    // User's input includes address or part of it
     char allowedLetters[100];
     char foundedCity[100]; // A buffer to store copy of line, if countPrefix == 1 (only one address in database)
     int countPrefix = 0;
